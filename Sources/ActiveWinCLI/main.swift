4178e0763b4aff42ecd91f9ce80156574601d930
Evgeny Sorokin <evgeny.sorokin91@gmail.com>
2023-12-12 23:35:27 +0100

    errors fixed

--- Sources/ActiveWinCLI/main.swift
+++ Sources/ActiveWinCLI/main.swift
@@ -2,7 +2,7 @@ import AppKit
 
 func getActiveBrowserTabURLAppleScriptCommand(_ appId: String) -> String? {
 	switch appId {
-	case "com.google.Chrome", "com.google.Chrome.beta", "com.google.Chrome.dev", "com.google.Chrome.canary", "com.brave.Browser", "com.brave.Browser.beta", "com.brave.Browser.nightly", "com.microsoft.edgemac", "com.microsoft.edgemac.Beta", "com.microsoft.edgemac.Dev", "com.microsoft.edgemac.Canary", "com.mighty.app", "com.ghostbrowser.gb1", "com.bookry.wavebox", "com.pushplaylabs.sidekick", "com.operasoftware.Opera",  "com.operasoftware.OperaNext", "com.operasoftware.OperaDeveloper", "com.vivaldi.Vivaldi", "company.thebrowser.Browser":
+	case "com.google.Chrome", "com.google.Chrome.beta", "com.google.Chrome.dev", "com.google.Chrome.canary", "com.brave.Browser", "com.brave.Browser.beta", "com.brave.Browser.nightly", "com.microsoft.edgemac", "com.microsoft.edgemac.Beta", "com.microsoft.edgemac.Dev", "com.microsoft.edgemac.Canary", "com.mighty.app", "com.ghostbrowser.gb1", "com.bookry.wavebox", "com.pushplaylabs.sidekick", "com.operasoftware.Opera",  "com.operasoftware.OperaNext", "com.operasoftware.OperaDeveloper", "com.vivaldi.Vivaldi":
 		return "tell app id \"\(appId)\" to get the URL of active tab of front window"
 	case "com.apple.Safari", "com.apple.SafariTechnologyPreview":
 		return "tell app id \"\(appId)\" to get URL of front document"
@@ -16,19 +16,37 @@ func exitWithoutResult() -> Never {
 	exit(0)
 }
 
-func printOutput(_ output: Any) -> Never {
-	guard let string = try? toJson(output) else {
-		exitWithoutResult()
-	}
-
-	print(string)
-	exit(0)
+let disableScreenRecordingPermission = CommandLine.arguments.contains("--no-screen-recording-permission")
+/*
+// Show accessibility permission prompt if needed. Required to get the complete window title.
+if !AXIsProcessTrustedWithOptions(["AXTrustedCheckOptionPrompt": true] as CFDictionary) {
+	print("active-win requires the accessibility permission in “System Preferences › Security & Privacy › Privacy › Accessibility”.")
+	exit(1)
 }
 
-func getWindowInformation(window: [String: Any], windowOwnerPID: pid_t) -> [String: Any]? {
+// Show screen recording permission prompt if needed. Required to get the complete window title.
+if !disableScreenRecordingPermission && !hasScreenRecordingPermission() {
+	print("active-win requires the screen recording permission in “System Preferences › Security & Privacy › Privacy › Screen Recording”.")
+	exit(1)
+}
+*/
+guard
+	let frontmostAppPID = NSWorkspace.shared.frontmostApplication?.processIdentifier,
+	let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID) as? [[String: Any]]
+else {
+	exitWithoutResult()
+}
+
+for window in windows {
+	let windowOwnerPID = window[kCGWindowOwnerPID as String] as! pid_t // Documented to always exist.
+
+	if windowOwnerPID != frontmostAppPID {
+		continue
+	}
+
 	// Skip transparent windows, like with Chrome.
 	if (window[kCGWindowAlpha as String] as! Double) == 0 { // Documented to always exist.
-		return nil
+		continue
 	}
 
 	let bounds = CGRect(dictionaryRepresentation: window[kCGWindowBounds as String] as! CFDictionary)! // Documented to always exist.
@@ -36,24 +54,19 @@ func getWindowInformation(window: [String: Any], windowOwnerPID: pid_t) -> [Stri
 	// Skip tiny windows, like the Chrome link hover statusbar.
 	let minWinSize: CGFloat = 50
 	if bounds.width < minWinSize || bounds.height < minWinSize {
-		return nil
+		continue
 	}
 
 	// This should not fail as we're only dealing with apps, but we guard it just to be safe.
 	guard let app = NSRunningApplication(processIdentifier: windowOwnerPID) else {
-		return nil
+		continue
 	}
 
 	let appName = window[kCGWindowOwnerName as String] as? String ?? app.bundleIdentifier ?? "<Unknown>"
 
 	let windowTitle = disableScreenRecordingPermission ? "" : window[kCGWindowName as String] as? String ?? ""
 
-	if app.bundleIdentifier == "com.apple.dock" {
-		return nil
-	}
-
 	var output: [String: Any] = [
-		"platform": "macos",
 		"title": windowTitle,
 		"id": window[kCGWindowNumber as String] as! Int, // Documented to always exist.
 		"bounds": [
@@ -80,52 +93,12 @@ func getWindowInformation(window: [String: Any], windowOwnerPID: pid_t) -> [Stri
 		output["url"] = url
 	}
 
-	return output
-}
-
-let disableScreenRecordingPermission = CommandLine.arguments.contains("--no-screen-recording-permission")
-let enableOpenWindowsList = CommandLine.arguments.contains("--open-windows-list")
-
-// Show accessibility permission prompt if needed. Required to get the complete window title.
-// if !AXIsProcessTrustedWithOptions(["AXTrustedCheckOptionPrompt": true] as CFDictionary) {
-// 	print("active-win requires the accessibility permission in “System Settings › Privacy & Security › Accessibility”.")
-// 	exit(1)
-// }
-
-// // Show screen recording permission prompt if needed. Required to get the complete window title.
-// if !disableScreenRecordingPermission && !hasScreenRecordingPermission() {
-// 	print("active-win requires the screen recording permission in “System Settings › Privacy & Security › Screen Recording”.")
-// 	exit(1)
-// }
-
-guard
-	let frontmostAppPID = NSWorkspace.shared.frontmostApplication?.processIdentifier,
-	let windows = CGWindowListCopyWindowInfo([.optionOnScreenOnly, .excludeDesktopElements], kCGNullWindowID) as? [[String: Any]]
-else {
-	exitWithoutResult()
-}
-
-var openWindows = [[String: Any]]();
-
-for window in windows {
-	let windowOwnerPID = window[kCGWindowOwnerPID as String] as! pid_t // Documented to always exist.
-	if !enableOpenWindowsList && windowOwnerPID != frontmostAppPID {
-		continue
+	guard let string = try? toJson(output) else {
+		exitWithoutResult()
 	}
 
-	guard let windowInformation = getWindowInformation(window: window, windowOwnerPID: windowOwnerPID) else {
-		continue
-	}
-
-	if !enableOpenWindowsList {
-		printOutput(windowInformation)
-	} else {
-		openWindows.append(windowInformation)
-	}
-}
-
-if !openWindows.isEmpty {
-	printOutput(openWindows)
+	print(string)
+	exit(0)
 }
 
 exitWithoutResult()
